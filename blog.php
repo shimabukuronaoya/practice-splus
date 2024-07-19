e24b4fd865858dfd3afc06b2e268225867f7521a
simabukuro <>
2024-07-19 18:48:38 +0900

    ブログ

---
+++ blog.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>splus</title>
+
+    <link rel="stylesheet" href="/assets/css/style.css">
+
+    <script src="/assets/js/jquery-3.6.1.min.js"></script>
+    <script src="/assets/js/slick.min.js"></script>
+    <script src="/assets/js/main.js"></script>
+</head>
+
+<body>
+    <header id="header" class="header l-section --wide">
+        <div class="l-content">
+            <h1 class="headerLogo">
+                <img src="/assets/img/logo.png" alt="splus" class="headerLogo__img">
+            </h1>
+            <div class="headerInfo">
+                <div class="headerInfo__left">
+                    <a href="#" class="headerInfo__link insta"><img src="/assets/img/ico_instagram.svg" alt="" class="headerInfo__linkIcon"></a>
+                    <a href="#" class="headerInfo__link faceBook"><img src="/assets/img/ico_facebook.svg" alt="" class="headerInfo__linkIcon"></a>
+                    <a href="#" class="headerInfo__link tel"><img src="/assets/img/ico_tel.svg" alt="" class="headerInfo__linkIcon">
+                        <span class="headerInfo__linkTel">090-9752-3301</span></a>
+                </div>
+                <div class="headerInfo__right">
+                    <a href="#" class="headerInfo__btn contact">
+                        お問い合わせ
+                    </a>
+                    <a href="#" class="headerInfo__btn reserve">
+                        予約する
+                    </a>
+                </div>
+            </div>
+            <nav class="headerNav">
+                <ul class="headerNavList --left">
+                    <li class="headerNavList__item">
+                        <a href="#" class="headerNavList__link">
+                            <span class="headerNavList__en">C-Card</span>
+                            <span class="headerNavList__ja">ライセンス講習</span>
+                        </a>
+                    </li>
+                    <li class="headerNavList__item">
+                        <a href="#" class="headerNavList__link">
+                            <span class="headerNavList__en">Fun Diving</span>
+                            <span class="headerNavList__ja">ファンダイビング</span>
+                        </a>
+                    </li>
+                    <li class="headerNavList__item">
+                        <a href="#" class="headerNavList__link">
+                            <span class="headerNavList__en">Experience Dive</span>
+                            <span class="headerNavList__ja">体験ダイビング</span>
+                        </a>
+                    </li>
+                </ul>
+                <ul class="headerNavList --right">
+                    <li class="headerNavList__item">
+                        <a href="#" class="headerNavList__link">
+                            <span class="headerNavList__en">Point Map</span>
+                            <span class="headerNavList__ja">ポイントマップ</span>
+                        </a>
+                    </li>
+                    <li class="headerNavList__item">
+                        <a href="#" class="headerNavList__link">
+                            <span class="headerNavList__en">Staff</span>
+                            <span class="headerNavList__ja">スタッフ紹介</span>
+                        </a>
+                    </li>
+                    <li class="headerNavList__item">
+                        <a href="#" class="headerNavList__link">
+                            <span class="headerNavList__en">Blog</span>
+                            <span class="headerNavList__ja">ブログ</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <div class="lowerKvArea blogKvArea l-section">
+        <img src="/assets/img/kv.jpg" alt="" class="topKvImg">
+        <div class="l-content">
+            <div class="topKvTxtBox">
+                <p class="pageTtl__en">Blog</p>
+                <h2 class="pageTtl__ja">ブログ</h2>
+            </div>
+        </div>
+    </div>
+
+    <section class="l-section blogArea">
+        <div class="l-content">
+            <ul class="blogList">
+                <li class="blogList__item">
+                    <a href="#" class="blogList__link">
+                        <div class="blogList__imgBox">
+                            <img src="/assets/img/blog/img_dummy01.jpg" alt="" class="blogList__img">
+                        </div>
+                        <p class="blogList__category">アドヴァンスド</p>
+                        <div class="blogList__txtBox">
+                            <h2 class="blogList__ttl">慶良間諸島でライセンス講習<span class="blogList__date">2024.07.15</span></h2>
+                        </div>
+                    </a>
+                </li>
+                <li class="blogList__item">
+                    <a href="#" class="blogList__link">
+                        <div class="blogList__imgBox">
+                            <img src="/assets/img/blog/img_dummy01.jpg" alt="" class="blogList__img">
+                        </div>
+                        <p class="blogList__category">アドヴァンスド</p>
+                        <div class="blogList__txtBox">
+                            <h2 class="blogList__ttl">慶良間諸島でライセンス講習<span class="blogList__date">2024.07.15</span></h2>
+                        </div>
+                    </a>
+                </li>
+                <li class="blogList__item">
+                    <a href="#" class="blogList__link">
+                        <div class="blogList__imgBox">
+                            <img src="/assets/img/blog/img_dummy01.jpg" alt="" class="blogList__img">
+                        </div>
+                        <p class="blogList__category">アドヴァンスド</p>
+                        <div class="blogList__txtBox">
+                            <h2 class="blogList__ttl">慶良間諸島でライセンス講習<span class="blogList__date">2024.07.15</span></h2>
+                        </div>
+                    </a>
+                </li>
+                <li class="blogList__item">
+                    <a href="#" class="blogList__link">
+                        <div class="blogList__imgBox">
+                            <img src="/assets/img/blog/img_dummy01.jpg" alt="" class="blogList__img">
+                        </div>
+                        <p class="blogList__category">アドヴァンスド</p>
+                        <div class="blogList__txtBox">
+                            <h2 class="blogList__ttl">慶良間諸島でライセンス講習<span class="blogList__date">2024.07.15</span></h2>
+                        </div>
+                    </a>
+                </li>
+                <li class="blogList__item">
+                    <a href="#" class="blogList__link">
+                        <div class="blogList__imgBox">
+                            <img src="/assets/img/blog/img_dummy01.jpg" alt="" class="blogList__img">
+                        </div>
+                        <p class="blogList__category">アドヴァンスド</p>
+                        <div class="blogList__txtBox">
+                            <h2 class="blogList__ttl">慶良間諸島でライセンス講習<span class="blogList__date">2024.07.15</span></h2>
+                        </div>
+                    </a>
+                </li>
+                <li class="blogList__item">
+                    <a href="#" class="blogList__link">
+                        <div class="blogList__imgBox">
+                            <img src="/assets/img/blog/img_dummy01.jpg" alt="" class="blogList__img">
+                        </div>
+                        <p class="blogList__category">アドヴァンスド</p>
+                        <div class="blogList__txtBox">
+                            <h2 class="blogList__ttl">慶良間諸島でライセンス講習<span class="blogList__date">2024.07.15</span></h2>
+                        </div>
+                    </a>
+                </li>
+                <li class="blogList__item">
+                    <a href="#" class="blogList__link">
+                        <div class="blogList__imgBox">
+                            <img src="/assets/img/blog/img_dummy01.jpg" alt="" class="blogList__img">
+                        </div>
+                        <p class="blogList__category">アドヴァンスド</p>
+                        <div class="blogList__txtBox">
+                            <h2 class="blogList__ttl">慶良間諸島でライセンス講習<span class="blogList__date">2024.07.15</span></h2>
+                        </div>
+                    </a>
+                </li>
+                <li class="blogList__item">
+                    <a href="#" class="blogList__link">
+                        <div class="blogList__imgBox">
+                            <img src="/assets/img/blog/img_dummy01.jpg" alt="" class="blogList__img">
+                        </div>
+                        <p class="blogList__category">アドヴァンスド</p>
+                        <div class="blogList__txtBox">
+                            <h2 class="blogList__ttl">慶良間諸島でライセンス講習<span class="blogList__date">2024.07.15</span></h2>
+                        </div>
+                    </a>
+                </li>
+                <li class="blogList__item">
+                    <a href="#" class="blogList__link">
+                        <div class="blogList__imgBox">
+                            <img src="/assets/img/blog/img_dummy01.jpg" alt="" class="blogList__img">
+                        </div>
+                        <p class="blogList__category">アドヴァンスド</p>
+                        <div class="blogList__txtBox">
+                            <h2 class="blogList__ttl">慶良間諸島でライセンス講習<span class="blogList__date">2024.07.15</span></h2>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+            <div class="blogPagination">
+                <ul class="page-number">
+                    <li><a class="prev page-numbers disabled"></a></li>
+                    <li><span aria-current="page" class="page-numbers current">1</span></li>
+                    <li><a class="page-numbers" href="#">2</a></li>
+                    <li><span class="page-numbers dots">…</span></li>
+                    <li><a class="page-numbers" href="#">22</a></li>
+                    <li><a class="next page-numbers" href="#"></a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="l-section breadArea">
+        <nav class="l-content breadNav">
+            <ul class="breadNavList">
+                <li class="breadNavList__item"><a href="/">TOP</a></li>
+                <li class="breadNavList__item"><a href="/">ブログ</a></li>
+            </ul>
+        </nav>
+    </section>
+
+    <footer class="footer">
+        <div class="pageTop"><a href="#" class="pageTop__link"><img src="https://www.splus-diving.com/wp-content/themes/splus/assets/img/common/img_page-top.svg" alt="pagetop" class="pageTop__img"></a></div>
+        <h1 class="footer__logo"><a href="/" class="footer__logoLink"><img src="https://www.splus-diving.com/wp-content/themes/splus/assets/img/common/logo.png" alt="splus okinawa diving service" class="footer__logoImg"></a></h1>
+        <div class="footerInfo">
+            <div class="headerInfo__left">
+                <a href="https://www.instagram.com/ito_yuichi78/" target="_blank" class="headerInfo__link insta">
+                    <img src="https://www.splus-diving.com/wp-content/themes/splus/assets/img/common/icon_insta.svg" alt="instagram" class="headerInfo__linkIcon">
+                </a>
+                <a href="https://www.facebook.com/profile.php?id=100011041908609" target="_blank" class="headerInfo__link fb">
+                    <img src="https://www.splus-diving.com/wp-content/themes/splus/assets/img/common/icon_fb.svg" alt="facebook" class="headerInfo__linkIcon">
+                </a>
+                <a href="tel:09097523301" class="headerInfo__link tel">
+                    <img src="https://www.splus-diving.com/wp-content/themes/splus/assets/img/common/icon_tel.svg" alt="tel" class="headerInfo__linkIcon"><span class="headerInfo__link-tel">090-9752-3301</span>
+                </a>
+            </div>
+            <div class="headerInfo__right">
+                <a href="#" class="headerInfo__btn contact">
+                    お問い合わせ
+                </a>
+                <a href="#" class="headerInfo__btn reserve">
+                    予約する
+                </a>
+            </div>
+        </div>
+        <div class="footer__copy"><small>Copyright © splus All Rights Reserved.</small></div>
+        <div class="footer__bitknot"><a href="https://bitknot.co.jp/" target="_blank" class="footer__bitknotLink"><img src="https://www.splus-diving.com/wp-content/themes/splus/assets/img/common/logo_bitknot.svg" alt="powerd by BITKNOT ビットノット株式会社" class="footer__bitknotLogo"></a></div>
+    </footer>
+
+    <script>
+        jQuery(function($) {
+            $(".slideAnimeList").slick({
+                autoplay: true, // 自動でスクロール
+                autoplay: true, // 自動でスクロール
+                autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
+                speed: 5000, // スライドが流れる速度を設定
+                cssEase: "linear", // スライドの流れ方を等速に設定
+                slidesToShow: 4, // 表示するスライドの数
+                swipe: false, // 操作による切り替えはさせない
+                arrows: false, // 矢印非表示
+                pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
+                pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
+                rtl: true, // スライダーを左から右に流す（逆向き）
+
+            });
+        });
+    </script>
+
+</body>
+
+</html>
